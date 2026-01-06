d13849bce8d4c5886d01f9b0e9dfd9e2d66cb627
son25 <thaisoncbq@gmail.com>
2026-01-06 10:05:27 +0700

    Remove text preprocessing for run qa

--- question-answering/run_qa.py
+++ question-answering/run_qa.py
@@ -41,11 +41,12 @@ from transformers import (
     TrainingArguments,
     default_data_collator,
     set_seed,
+    RobertaTokenizerFast # Import thêm để hỗ trợ PhoBERT
 )
 from transformers.utils import check_min_version
 from transformers.utils.versions import require_version
-from transformers import RobertaTokenizerFast
-from text_preprocess import TextNormalize, convert_unicode
+
+
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.57.0.dev0")
 
@@ -263,15 +264,7 @@ def main():
     # Set seed before initializing model.
     set_seed(training_args.seed)
 
-    # Get the datasets: you can either provide your own CSV/JSON/TXT training and evaluation files (see below)
-    # or just provide the name of one of the public datasets available on the hub at https://huggingface.co/datasets/
-    # (the dataset will be downloaded automatically from the datasets Hub).
-    #
-    # For CSV/JSON files, this script will use the column called 'text' or the first column if no column called
-    # 'text' is found. You can easily tweak this behavior (see below).
-    #
-    # In distributed training, the load_dataset function guarantee that only one local process can concurrently
-    # download the dataset.
+    # Get the datasets
     if data_args.dataset_name is not None:
         # Downloading and loading a dataset from the hub.
         raw_datasets = load_dataset(
@@ -300,14 +293,8 @@ def main():
             cache_dir=model_args.cache_dir,
             token=model_args.token,
         )
-    # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
-    # https://huggingface.co/docs/datasets/loading_datasets.
 
     # Load pretrained model and tokenizer
-    #
-    # Distributed training:
-    # The .from_pretrained methods guarantee that only one local process can concurrently
-    # download model & vocab.
     config = AutoConfig.from_pretrained(
         model_args.config_name if model_args.config_name else model_args.model_name_or_path,
         cache_dir=model_args.cache_dir,
@@ -315,7 +302,9 @@ def main():
         token=model_args.token,
         trust_remote_code=model_args.trust_remote_code,
     )
-    if "phobert" in model_args.model_name_or_path: # QUAN TRỌNG
+
+    # --- ĐOẠN CODE CẦN GIỮ LẠI ĐỂ CHẠY PHOBERT ---
+    if "phobert" in model_args.model_name_or_path: 
         tokenizer = RobertaTokenizerFast.from_pretrained(
             model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
             cache_dir=model_args.cache_dir,
@@ -325,7 +314,6 @@ def main():
             trust_remote_code=model_args.trust_remote_code,
         )
     else:   
-        # Các model khác chạy bình thường
         tokenizer = AutoTokenizer.from_pretrained(
             model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
             cache_dir=model_args.cache_dir,
@@ -334,6 +322,8 @@ def main():
             token=model_args.token,
             trust_remote_code=model_args.trust_remote_code,
         )
+    # ----------------------------------------------
+
     model = AutoModelForQuestionAnswering.from_pretrained(
         model_args.model_name_or_path,
         from_tf=bool(".ckpt" in model_args.model_name_or_path),
@@ -343,9 +333,12 @@ def main():
         token=model_args.token,
         trust_remote_code=model_args.trust_remote_code,
     )
-    model.resize_token_embeddings(len(tokenizer)) # QUAN TRỌNG
+
+    # --- GIỮ LẠI ĐOẠN RESIZE ---
+    model.resize_token_embeddings(len(tokenizer)) 
+    # ---------------------------
+
     # Tokenizer check: this script requires a fast tokenizer.
-    # Check if tokenizer has _tokenizer attribute (from tokenizers library) or is_fast property
     if not (hasattr(tokenizer, "_tokenizer") or getattr(tokenizer, "is_fast", False)):
         raise TypeError(
             "This example script only works for models that have a fast tokenizer. Check out the big table of models at"
@@ -354,7 +347,6 @@ def main():
         )
 
     # Preprocessing the datasets.
-    # Preprocessing is slightly different for training and evaluation.
     if training_args.do_train:
         column_names = raw_datasets["train"].column_names
     elif training_args.do_eval:
@@ -368,60 +360,8 @@ def main():
     # Padding side determines if we do (question|context) or (context|question).
     pad_on_right = tokenizer.padding_side == "right"
     
-    # Text Normalization
-    logger.info("Start normalizing the dataset...")
-    
-    # Khởi tạo class xử lý từ module của bạn
-    normalizer = TextNormalize()
-    normalizer.createVowelsTable() # Bắt buộc gọi để tạo bảng nguyên âm
+    # --- ĐÃ XÓA PHẦN TEXT NORMALIZATION GÂY LỖI ---
 
-    def preprocess_dataset_function(examples):
-        """Hàm chuẩn hóa chạy trên từng batch dữ liệu"""
-        questions = examples[question_column_name]
-        contexts = examples[context_column_name]
-        
-        new_questions = []
-        new_contexts = []
-        
-        for q, c in zip(questions, contexts):
-            # 1. Xử lý Question
-            q = convert_unicode(q)          # Chuyển mã Unicode dựng sẵn/tổ hợp
-            q = normalizer.normalize(q)     # Lowercase, xóa emoji, xóa dấu câu thừa
-            # Chuẩn hóa dấu câu tiếng Việt cho từng từ
-            q = " ".join([normalizer.WordStandardized(word) for word in q.split()])
-            new_questions.append(q)
-            
-            # 2. Xử lý Context
-            c = convert_unicode(c)
-            c = normalizer.normalize(c)
-            c = " ".join([normalizer.WordStandardized(word) for word in c.split()])
-            new_contexts.append(c)
-            
-        examples[question_column_name] = new_questions
-        examples[context_column_name] = new_contexts
-        return examples
-
-    # Áp dụng map vào toàn bộ dataset (Train, Val, Test)
-    # Lưu ý: batched=True giúp chạy nhanh hơn
-    if "train" in raw_datasets:
-        raw_datasets["train"] = raw_datasets["train"].map(
-            preprocess_dataset_function, 
-            batched=True, 
-            desc="Normalizing Train Data"
-        )
-    if "validation" in raw_datasets:
-        raw_datasets["validation"] = raw_datasets["validation"].map(
-            preprocess_dataset_function, 
-            batched=True, 
-            desc="Normalizing Validation Data"
-        )
-    if "test" in raw_datasets:
-        raw_datasets["test"] = raw_datasets["test"].map(
-            preprocess_dataset_function, 
-            batched=True, 
-            desc="Normalizing Test Data"
-        )
-    
     if data_args.max_seq_length > tokenizer.model_max_length:
         logger.warning(
             f"The max_seq_length passed ({data_args.max_seq_length}) is larger than the maximum length for the "
@@ -450,19 +390,13 @@ def main():
             padding="max_length" if data_args.pad_to_max_length else False,
         )
 
-        # Since one example might give us several features if it has a long context, we need a map from a feature to
-        # its corresponding example. This key gives us just that.
         sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
-        # The offset mappings will give us a map from token to character position in the original context. This will
-        # help us compute the start_positions and end_positions.
         offset_mapping = tokenized_examples.pop("offset_mapping")
 
-        # Let's label those examples!
         tokenized_examples["start_positions"] = []
         tokenized_examples["end_positions"] = []
 
         for i, offsets in enumerate(offset_mapping):
-            # We will label impossible answers with the index of the CLS token.
             input_ids = tokenized_examples["input_ids"][i]
             if tokenizer.cls_token_id in input_ids:
                 cls_index = input_ids.index(tokenizer.cls_token_id)
@@ -471,38 +405,30 @@ def main():
             else:
                 cls_index = 0
 
-            # Grab the sequence corresponding to that example (to know what is the context and what is the question).
             sequence_ids = tokenized_examples.sequence_ids(i)
 
-            # One example can give several spans, this is the index of the example containing this span of text.
             sample_index = sample_mapping[i]
             answers = examples[answer_column_name][sample_index]
-            # If no answers are given, set the cls_index as answer.
+            
             if len(answers["answer_start"]) == 0:
                 tokenized_examples["start_positions"].append(cls_index)
                 tokenized_examples["end_positions"].append(cls_index)
             else:
-                # Start/end character index of the answer in the text.
                 start_char = answers["answer_start"][0]
                 end_char = start_char + len(answers["text"][0])
 
-                # Start token index of the current span in the text.
                 token_start_index = 0
                 while sequence_ids[token_start_index] != (1 if pad_on_right else 0):
                     token_start_index += 1
 
-                # End token index of the current span in the text.
                 token_end_index = len(input_ids) - 1
                 while sequence_ids[token_end_index] != (1 if pad_on_right else 0):
                     token_end_index -= 1
 
-                # Detect if the answer is out of the span (in which case this feature is labeled with the CLS index).
                 if not (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
                     tokenized_examples["start_positions"].append(cls_index)
                     tokenized_examples["end_positions"].append(cls_index)
                 else:
-                    # Otherwise move the token_start_index and token_end_index to the two ends of the answer.
-                    # Note: we could go after the last offset if the answer is the last word (edge case).
                     while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
                         token_start_index += 1
                     tokenized_examples["start_positions"].append(token_start_index - 1)
@@ -517,10 +443,9 @@ def main():
             raise ValueError("--do_train requires a train dataset")
         train_dataset = raw_datasets["train"]
         if data_args.max_train_samples is not None:
-            # We will select sample from whole data if argument is specified
             max_train_samples = min(len(train_dataset), data_args.max_train_samples)
             train_dataset = train_dataset.select(range(max_train_samples))
-        # Create train feature from dataset
+        
         with training_args.main_process_first(desc="train dataset map pre-processing"):
             train_dataset = train_dataset.map(
                 prepare_train_features,
@@ -531,20 +456,13 @@ def main():
                 desc="Running tokenizer on train dataset",
             )
         if data_args.max_train_samples is not None:
-            # Number of samples might increase during Feature Creation, We select only specified max samples
             max_train_samples = min(len(train_dataset), data_args.max_train_samples)
             train_dataset = train_dataset.select(range(max_train_samples))
 
     # Validation preprocessing
     def prepare_validation_features(examples):
-        # Some of the questions have lots of whitespace on the left, which is not useful and will make the
-        # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
-        # left whitespace
         examples[question_column_name] = [q.lstrip() for q in examples[question_column_name]]
 
-        # Tokenize our examples with truncation and maybe padding, but keep the overflows using a stride. This results
-        # in one example possible giving several features when a context is long, each of those features having a
-        # context that overlaps a bit the context of the previous feature.
         tokenized_examples = tokenizer(
             examples[question_column_name if pad_on_right else context_column_name],
             examples[context_column_name if pad_on_right else question_column_name],
@@ -556,25 +474,16 @@ def main():
             padding="max_length" if data_args.pad_to_max_length else False,
         )
 
-        # Since one example might give us several features if it has a long context, we need a map from a feature to
-        # its corresponding example. This key gives us just that.
         sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
-
-        # For evaluation, we will need to convert our predictions to substrings of the context, so we keep the
-        # corresponding example_id and we will store the offset mappings.
         tokenized_examples["example_id"] = []
 
         for i in range(len(tokenized_examples["input_ids"])):
-            # Grab the sequence corresponding to that example (to know what is the context and what is the question).
             sequence_ids = tokenized_examples.sequence_ids(i)
             context_index = 1 if pad_on_right else 0
 
-            # One example can give several spans, this is the index of the example containing this span of text.
             sample_index = sample_mapping[i]
             tokenized_examples["example_id"].append(examples["id"][sample_index])
 
-            # Set to None the offset_mapping that are not part of the context so it's easy to determine if a token
-            # position is part of the context or not.
             tokenized_examples["offset_mapping"][i] = [
                 (o if sequence_ids[k] == context_index else None)
                 for k, o in enumerate(tokenized_examples["offset_mapping"][i])
@@ -587,10 +496,9 @@ def main():
             raise ValueError("--do_eval requires a validation dataset")
         eval_examples = raw_datasets["validation"]
         if data_args.max_eval_samples is not None:
-            # We will select sample from whole data
             max_eval_samples = min(len(eval_examples), data_args.max_eval_samples)
             eval_examples = eval_examples.select(range(max_eval_samples))
-        # Validation Feature Creation
+        
         with training_args.main_process_first(desc="validation dataset map pre-processing"):
             eval_dataset = eval_examples.map(
                 prepare_validation_features,
@@ -601,7 +509,6 @@ def main():
                 desc="Running tokenizer on validation dataset",
             )
         if data_args.max_eval_samples is not None:
-            # During Feature creation dataset samples might increase, we will select required samples again
             max_eval_samples = min(len(eval_dataset), data_args.max_eval_samples)
             eval_dataset = eval_dataset.select(range(max_eval_samples))
 
@@ -610,9 +517,8 @@ def main():
             raise ValueError("--do_predict requires a test dataset")
         predict_examples = raw_datasets["test"]
         if data_args.max_predict_samples is not None:
-            # We will select sample from whole data
             predict_examples = predict_examples.select(range(data_args.max_predict_samples))
-        # Predict Feature Creation
+        
         with training_args.main_process_first(desc="prediction dataset map pre-processing"):
             predict_dataset = predict_examples.map(
                 prepare_validation_features,
@@ -623,13 +529,10 @@ def main():
                 desc="Running tokenizer on prediction dataset",
             )
         if data_args.max_predict_samples is not None:
-            # During Feature creation dataset samples might increase, we will select required samples again
             max_predict_samples = min(len(predict_dataset), data_args.max_predict_samples)
             predict_dataset = predict_dataset.select(range(max_predict_samples))
 
     # Data collator
-    # We have already padded to max length if the corresponding flag is True, otherwise we need to pad in the data
-    # collator.
     data_collator = (
         default_data_collator
         if data_args.pad_to_max_length
@@ -638,7 +541,6 @@ def main():
 
     # Post-processing:
     def post_processing_function(examples, features, predictions, stage="eval"):
-        # Post-processing: we match the start logits and end logits to answers in the original context.
         predictions = postprocess_qa_predictions(
             examples=examples,
             features=features,
@@ -651,7 +553,6 @@ def main():
             log_level=log_level,
             prefix=stage,
         )
-        # Format the result to the format the metric expects.
         if data_args.version_2_with_negative:
             formatted_predictions = [
                 {"id": str(k), "prediction_text": v, "no_answer_probability": 0.0} for k, v in predictions.items()
